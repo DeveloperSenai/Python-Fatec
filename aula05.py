4be5d286a99ae166de2c9fd8d587f41ec37429b4
Daniel Cerri <daniel.cerri@docente.senai.br>
2025-11-03 11:29:16 -0300

    Add files via upload

--- aula05.py
+++ aula05.py
@@ -39,7 +39,64 @@ os.system("color 6")
 
 # for i in range(1,11):
 #     print(f"{numero} X {i} = {numero*i}")
-  
-  
 
 
+#padaria
+
+
+# print("*"*30,"PADARIA PAO DE ONTEM","*"*30)
+
+# precoPao = float(input("Digite o preço do pao: "))
+
+# for i in range(1,51):
+#     print(i , f" - R$: {precoPao*i:.2f}")
+
+# qtd = int(input("Digite a quantidade que gostaria de levar: "))
+# total = precoPao*qtd
+
+# print(f"TOTAL DA COMPRA SEM DESCONTO  R$: {total:.2f}")
+
+# cupom = input("Digite um cupom de desconto: ").lower()
+
+# if cupom == "paomurcho":
+#     total = total -(total*0.10)
+#     print("CUPOM VALIDO APLICADO 10% DE DESCONTO!")
+# else:
+#     print("CUPOM INVALIDO DESCONTO NAO APLICADO!")
+     
+# print(f"TOTAL DA COMPRA COM/SEM CUPOM R$: {total:.2f}")
+
+# pagto = int(input("DIGITE A FORMA DE PAGTO (APENAS O VALOR): NOTA 2 | NOTA 5 | NOTA 10 | NOTA 50: "))
+
+# if pagto > total:
+#     print(f"TROCO DE R$: {pagto-total:.2f}")
+# elif pagto < total:
+#     print(f" DINHEIRO INSULFICIENTE , FALTAM R$: {total-pagto:.2f}")
+# else:
+#     print("NÃO TEM TROCO, PAGAMENTO OK!")
+    
+    
+    
+#desafio ! 
+
+#Faça um programa que leia 5 números 
+# e informe o maior número. 
+
+#DICA USEM 2 VARIAVEIS
+#NUMERO
+#MAIORNUMERO
+
+maiornumero=0
+numero=0
+
+
+for i in range(5):
+    numero = float(input("Digite um numero: "))
+    
+    # if i ==0:  #- validação para numeros negativos!
+    #     maiornumero=numero 
+    
+    if numero > maiornumero:
+        maiornumero = numero
+
+print(maiornumero)
