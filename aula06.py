249a5a9fb9c7294d06edf98135280d692dbec58c
Daniel Cerri <daniel.cerri@docente.senai.br>
2025-11-10 11:29:42 -0300

    Add files via upload

---
+++ aula06.py
@@ -0,0 +1,77 @@
+import os
+os.system("cls")
+os.system("color 6")
+
+
+#ESTRUTURA DE REPETIÇÃO WHILE (ENQUANTO)
+#ENQUANTO UMA CONDIÇÃO FOR VERDADEIRA ELE EXECUTA O LOPPING ATÉ QUE ELA SEJA FALSA
+#POR SE TRATAR DE UMA CONDIÇÃO VOCE USAR OS OPERADORES CONDICIONAIS: 
+#OPERADORES: < <= > >= == != AND OR
+
+
+# aula=6
+
+# while aula==6:
+#     print("APRENDENDO WHILE")
+#     aula=7
+#     #break -> quebra o loop
+# else:
+#    print("ACABOU O WHILE!")
+ 
+#PARA SAIR DO WHILE A CONDIÇÃO DEVE SER FALSA OU UTILIZE A PALAVRA BREAK
+
+#while incremental
+# i=0
+
+# while i <=10:
+#     print(i)
+#     #i = i +1 # incrementando uma variavel por extenso
+#     i+=1 # A mesma coisa que i=i+1 (abreviado)
+
+
+#ex1
+# n= 0
+# while n!=20:
+#     print(n)
+#     n+=1
+
+# #ex2
+# n = 25
+# while n<=50:
+#     print(n)
+#     n+=1
+
+# print("PROGRAMA DE CADASTRO")
+# print("""
+# 1-Cadastro do nome
+# 2-Cadastro do cpf
+# 3-Cadastro da idade
+# 4-Sai do programa
+# """)
+
+# opcao=0
+# while opcao !=4:
+#     opcao = int(input("Digite a opção desejada: "))
+    
+#     if opcao ==1:
+#         nome = input("Digite seu nome: ")
+#     elif opcao ==2:
+#         cpf = input("Digite seu cpf: ")
+#     elif opcao ==3:
+#         idade = input("Digite sua idade: ")
+# else:
+#     print("DADOS CADASTRADOS!")
+#     print(f"""
+# NOME:{nome}
+# CPF:{cpf}
+# IDADE:{idade} 
+# """)
+
+
+
+import random 
+
+numero = random.randint(0,5) #GERA UM NUMERO ALEATORIO ENTRE 0 E 5
+print(numero)
+
+
